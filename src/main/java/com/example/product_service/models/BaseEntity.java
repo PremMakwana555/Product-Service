5d3bbec7031d24255fe8bbc5941026d7f812eb9e
Prem Makwana <prem.makwana@1mg.com>
2024-09-02 11:59:07 +0530

    added jpa, models, services and repository

---
+++ src/main/java/com/example/product_service/models/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.example.product_service.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Date;
+
+@EnableJpaAuditing
+@MappedSuperclass
+@Data
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreatedDate
+    private Date createdAt;
+    @LastModifiedDate
+    private Date updatedAt;
+    @CreatedBy
+    private String createdBy;
+    @LastModifiedBy
+    private String updatedBy;
+}
