e628c14aafb9b1bacdc455732ef15be6a3546b0b
PremMakwana555 <premmakwana555@gmail.com>
2026-01-11 11:05:08 +0530

    fix: expose product ID in DTO

--- src/main/java/com/example/product_service/dto/ProductDto.java
+++ src/main/java/com/example/product_service/dto/ProductDto.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class ProductDto {
+    private Long id;
     private String name;
     private String description;
     private BigDecimal price;
